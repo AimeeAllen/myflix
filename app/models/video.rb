ca99ea8696f423c0858ad14d36bd78e4ff336f65
Aimee Legge <aimee.legge@gmail.com>
2016-03-11 18:46:43 +1300

    implemented search by title

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   validates :title, :description, presence: true
+
+  def self.search_by_title(search_term)
+    return [] if search_term.empty?
+    where("lower(title) LIKE ?", "%#{search_term.downcase}%").order("created_at DESC")
+  end
 end
